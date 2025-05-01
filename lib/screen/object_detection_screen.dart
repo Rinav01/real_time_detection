9dc74f4c123a6bee974768e0d3427739198fc85a
Rinav01 <143955531+Rinav01@users.noreply.github.com>
2025-05-02 01:24:03 +0530

    Add files via upload

--- lib/screen/object_detection_screen.dart
+++ lib/screen/object_detection_screen.dart
@@ -1,16 +1,16 @@
-// ignore_for_file: depend_on_referenced_packages
-
-import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+
+import 'dart:io';
+import 'dart:async';
 import 'package:path_provider/path_provider.dart';
-import 'package:real_time_detection/utils/my_custom_text_style.dart';
-import 'package:tflite_flutter/tflite_flutter.dart';
-import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
+
+import '../utils/my_custom_text_style.dart';
 
 class ObjectDetectionScreen extends StatefulWidget {
   final List<CameraDescription> cameras;
+
   const ObjectDetectionScreen({super.key, required this.cameras});
 
   @override
@@ -21,37 +21,44 @@ class _ObjectDetectionScreenState extends State<ObjectDetectionScreen> {
   late CameraController _cameraController;
   bool isCameraReady = false;
   String result = "Detecting...";
-  late Interpreter _interpreter;
+  late ImageLabeler _imageLabeler;
   bool isDetecting = false;
 
   @override
   void initState() {
     super.initState();
     _initializeCamera();
-    _initializeInterpreter();
+    _initializeMLKit();
   }
 
+  /// Initialize Camera
   Future<void> _initializeCamera() async {
     _cameraController = CameraController(
-        widget.cameras[0], ResolutionPreset.high,
-        enableAudio: false);
+      widget.cameras[0],
+      ResolutionPreset.ultraHigh,
+      enableAudio: false,
+    );
+
     await _cameraController.initialize();
     if (!mounted) return;
+
     setState(() {
       isCameraReady = true;
     });
-    startImageStream();
+
+    // Start Streaming for Real-time Detection
+    _startImageStream();
   }
 
-  Future<void> _initializeInterpreter() async {
-    try {
-      _interpreter = await Interpreter.fromAsset('model.tflite');
-    } catch (e) {
-      print("Error loading model: $e");
-    }
+  /// Initialize ML Kit
+  void _initializeMLKit() {
+    _imageLabeler = ImageLabeler(
+      options: ImageLabelerOptions(confidenceThreshold: 0.5),
+    );
   }
 
-  void startImageStream() {
+  /// Start Image Stream for Real-time Detection
+  void _startImageStream() {
     _cameraController.startImageStream((CameraImage image) async {
       if (isDetecting) return;
       isDetecting = true;
@@ -60,87 +67,125 @@ class _ObjectDetectionScreenState extends State<ObjectDetectionScreen> {
     });
   }
 
+  /// Process Camera Frame
   Future<void> _processImage(CameraImage cameraImage) async {
     try {
       final Directory tempDir = await getTemporaryDirectory();
       final String filePath =
-          "${tempDir.path}/${DateTime.now().microsecondsSinceEpoch}.jpg";
+          '${tempDir.path}/${DateTime.now().millisecondsSinceEpoch}.jpg';
       final File imageFile = File(filePath);
 
+      // Convert CameraImage to File
       final XFile picture = await _cameraController.takePicture();
-      await picture.saveTo(filePath);
+      await picture.saveTo(imageFile.path);
 
-      final inputImage = InputImage.fromFilePath(filePath);
+      final inputImage = InputImage.fromFile(imageFile);
+      final List<ImageLabel> labels = await _imageLabeler.processImage(
+        inputImage,
+      );
 
-      // Preprocess the image and run inference
-      final TensorImage tensorImage = TensorImage.fromFile(imageFile);
-      final TensorBuffer outputBuffer =
-          TensorBuffer.createFixedSize([1, 1001], TfLiteType.float32);
-      _interpreter.run(tensorImage.buffer, outputBuffer.buffer);
-
-      // Process the output and update the result
-      final List<String> labels =
-          await FileUtil.loadLabels('assets/labels.txt');
-      final List<double> probabilities = outputBuffer.getDoubleList();
-      final List<String> detectedObjects = [];
-      for (int i = 0; i < probabilities.length; i++) {
-        if (probabilities[i] > 0.5) {
-          detectedObjects.add(
-              "${labels[i]} - ${(probabilities[i] * 100).toStringAsFixed(2)}%");
-        }
-      }
+      String detectedObjects =
+      labels.isNotEmpty
+          ? labels
+          .map(
+            (label) =>
+        "${label.label} - ${(label.confidence * 100).toStringAsFixed(2)}%",
+      )
+          .join("\n")
+          : "No object detected";
 
       setState(() {
-        result = detectedObjects.isNotEmpty
-            ? detectedObjects.join("\n")
-            : "No Object Detected";
+        result = detectedObjects;
       });
-      print("Detected Objects: $result");
-    } catch (error) {
-      print("Error Processing Image: $error");
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text("Error Processing Image"),
-        backgroundColor: Colors.red,
-      ));
+    } catch (e) {
+      print("Error processing image: $e");
     }
   }
 
   @override
+  void dispose() {
+    _cameraController.dispose();
+    _imageLabeler.close();
+    super.dispose();
+  }
+
+  MediaQueryData? mqData;
+  @override
   Widget build(BuildContext context) {
+    mqData = MediaQuery.of(context);
     return Scaffold(
+      /// -------------- Appbar --------------------- ///
       appBar: AppBar(
+        backgroundColor: Color(0xff213555),
         title: Text(
-          "Object Detection",
-          style: myTextStyle24(
-              fontWeight: FontWeight.bold, fontColors: Colors.white),
+          "Real-time Object Detection",
+          style: TextStyle(color: Colors.white, fontSize: 18),
         ),
-        backgroundColor: const Color(0xff022246),
         centerTitle: true,
+        leading: Image.asset("assets/icons/object.png" , color: Colors.blue,),
       ),
-      backgroundColor: const Color(0xff022246),
+      backgroundColor: Color(0xff3E5879),
+
+      ///----------------- BODY --------------------///
       body: SingleChildScrollView(
         child: Stack(
           children: [
+            /// Camera Preview
             Center(
               child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: isCameraReady
+                width: mqData!.size.width,
+
+                child:
+                isCameraReady
                     ? CameraPreview(_cameraController)
-                    : const Center(child: CircularProgressIndicator()),
+                    : Center(child: CircularProgressIndicator()),
               ),
             ),
+
+            /// Detection Result
             Positioned(
-              bottom: 20,
-              left: 20,
-              child: Text(
-                result,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  backgroundColor: Colors.black54,
+              bottom: 0,
+              child: Container(
+                width: mqData!.size.width,
+
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Color(0x5af0bb78),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Detected Objects",
+                      style: myTextStyle18(
+                        fontWeight: FontWeight.bold,
+                        fontColors: Colors.orange,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(color: Colors.black12, blurRadius: 4),
+                        ],
+                      ),
+                      child: Text(
+                        result,
+                        textAlign: TextAlign.start,
+                        style: myTextStyle18(),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
+
           ],
         ),
       ),
